7acfc5e362f3c1e00b035e47fab75fdd83e37324
Diego Nunes <contato@diegonunes.dev>
2020-07-30 23:25:12 -0300

    🚧 display images

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
+
+let imagesArray = [];
+
 const count = 10;
 const apiKEY = '6UxV8mqdsUU9HBYcDlmbJz9j6SnJlNc7hbvXwNJjQ3Q';
 const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKEY}&count=${count}`;
 
-async function getPhotos() {
+function setAttribute(element, attribute) {
+  for (const key in attribute) {
+    element.setAttribute(key, attribute[key]);
+  }
+}
+
+function displayImages() {
+  imagesArray.forEach((image) => {
+    const item = document.createElement('a');
+    setAttribute(item, {
+      href: image.links.html,
+      target: '_blank',
+    });
+
+    const img = document.createElement('img');
+    setAttribute(img, {
+      src: image.urls.regular,
+      alt: image.alt_description,
+      title: image.alt_description,
+    });
+
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
+}
+
+async function getImages() {
   try {
     const response = await fetch(apiURL);
-    const data = await response.json();
-    console.log(data);
+    imagesArray = await response.json();
+    displayImages();
   } catch (error) {
     console.log(error);
   }
 }
 
-getPhotos();
+getImages();
